614b766849d72abaf98a7d3e297ec3a35d391aa3
mcondemaita <mariacondemaita@gmail.com>
2021-09-19 15:28:00 -0400

    Initial Commit

---
+++ Flix1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix1
+//
+//  Created by Maria  Condemaita on 9/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
